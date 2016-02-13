63d2346d2249125cb38a66e9e913362f724dd54a
Cyuss <maamra.youcef@gmail.com>
2016-02-13 15:52:35 +0100

    just save

--- mark.py
+++ mark.py
@@ -15,29 +15,46 @@ def section(caption):
     print('\n\n' + str(caption))
     print('-' * len(caption)) 
 
+'''
+    load data
+'''
 def load_data(fileName):
     df = pd.read_csv(fileName, sep = ",")
     return df
 
+'''
+    compute rates of returns matrix
+'''
 def rates_return(prices) :
     prices = prices.as_matrix()
     dim = prices.shape
     nb_rows, nb_cols = dim[0], dim[1] 
     rate_ret = np.zeros((nb_rows - 1, nb_cols))
+
     # p[i, j] = (p[i+1, j] - p[i, j]) / p[i, j]
     for i in range(0, nb_rows-1) :
         for j in range(0, nb_cols) :
             rate_ret[i, j] = (prices[i+1, j] - prices[i, j]) / prices[i, j]
     return rate_ret
 
+'''
+    compute covariance matrix
+'''
 def cov_matrix(rates) :
     cov_mat = np.cov(rates.T)
     return cov_mat
 
+'''
+    compute expected returns vector
+'''
 def exp_returns(rates) :
     exp_rets = pd.Series(np.mean(rates, axis = 0))
     return exp_rets
 
+
+'''
+    markowitz portfolio function for solving the convex programm
+'''
 def markwoitz_portfolio(rate_rets, cov_mat, exp_rets, target_ret = 0.0006, allow_short = True, lmin = 0, lmax = 1) :
     # matrices conversion : P = covariance
     n = len(cov_mat)
@@ -53,7 +70,6 @@ def markwoitz_portfolio(rate_rets, cov_mat, exp_rets, target_ret = 0.0006, allow
         # exp_rets * x >= 1
         G = opt.matrix(-exp_rets.values).T
         h = opt.matrix(-target_ret)
-    
     A = opt.matrix(1.0, (1, n))
     x = opt.matrix(1.0, (n, 1))
     b = opt.matrix(1.0)
@@ -68,11 +84,8 @@ def markwoitz_portfolio(rate_rets, cov_mat, exp_rets, target_ret = 0.0006, allow
     weights = pd.Series(sol['x'])
     ret = (opt.matrix(weights).T * opt.matrix(exp_rets))[0, 0]
     risk = np.sqrt(np.asmatrix(weights) * cov_mat * np.asmatrix(weights).T)
-    #risk = [np.sqrt(blas.dot(np.asmatrix(x).T, cov_mat*np.asmatrix(x))) for x in weights]
-    #print "here type"
-    #print type(risk)
+    
     return weights, ret, risk, cov_mat, exp_rets
-    #return cov_mat, exp_rets
 
 def main() :
     # loading data
@@ -95,13 +108,13 @@ def main() :
 
     for r in space :
         k, x, y, t1, t2 = markwoitz_portfolio(rateret, covmat, exprets, target_ret = r, allow_short = True)
-        lx.append(x)
-        ly.append(y)
+        lx.append(x*10000)
+        ly.append(y*100)
 
     #print len(ly)
     z = zip(lx, ly)
 
-    plt.scatter(ly, lx, marker = 'o')
+    plt.scatter(ly, lx, marker = 'o', c = 'c')
     plt.xlabel('std')
     plt.ylabel('mean')
     plt.show()
