bdda331c01623abf000ad5feda2dd771943be9d7
Cyuss <maamra.youcef@gmail.com>
2016-02-13 15:28:55 +0100

    add main function

--- mark.py
+++ mark.py
@@ -74,34 +74,36 @@ def markwoitz_portfolio(rate_rets, cov_mat, exp_rets, target_ret = 0.0006, allow
     return weights, ret, risk, cov_mat, exp_rets
     #return cov_mat, exp_rets
 
+def main() :
+    # loading data
+    df = load_data("prices.csv")
+    d = df.head(2518)
+    d = d[['AA', 'AXP', 'CAT', 'DD']]
 
-# loading data
-df = load_data("prices.csv")
-d = df.head(2518)
-d = d[['AA', 'AXP', 'CAT', 'DD']]
+    # compute covariance matrix, rates of returns and expected returns
+    rateret = rates_return(d)
+    covmat = cov_matrix(rateret)
+    exprets = exp_returns(rateret)
 
-# compute covariance matrix, rates of returns and expected returns
-rateret = rates_return(d)
-covmat = cov_matrix(rateret)
-exprets = exp_returns(rateret)
+    k, x, y, sd, me = markwoitz_portfolio(rateret, covmat, exprets, allow_short = False)
 
-k, x, y, sd, me = markwoitz_portfolio(rateret, covmat, exprets, allow_short = False)
+    y = np.asarray(y)
+    lx = []
+    ly = []
 
-y = np.asarray(y)
-lx = []
-ly = []
+    space = np.arange(.0001, .0008, .000001)
 
-space = np.arange(.0001, .0008, .000001)
+    for r in space :
+        k, x, y, t1, t2 = markwoitz_portfolio(rateret, covmat, exprets, target_ret = r, allow_short = True)
+        lx.append(x)
+        ly.append(y)
 
-for r in space :
-    k, x, y, t1, t2 = markwoitz_portfolio(rateret, covmat, exprets, target_ret = r, allow_short = True)
-    lx.append(x*10000)
-    ly.append(y*100)
+    #print len(ly)
+    z = zip(lx, ly)
 
-#print len(ly)
-z = zip(lx, ly)
+    plt.scatter(ly, lx, marker = 'o')
+    plt.xlabel('std')
+    plt.ylabel('mean')
+    plt.show()
 
-plt.scatter(ly, lx, marker = 'o')
-plt.xlabel('std')
-plt.ylabel('mean')
-plt.show()
+main()
